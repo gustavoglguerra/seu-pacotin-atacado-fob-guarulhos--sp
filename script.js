517b17fe4af61b56823676ad01f332805610442b
gustavoglguerra <gustavoglguerra@gmail.com>
2026-02-01 11:15:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,16 @@
-alert("JS NOVO CARREGOU");
-
 function calcular() {
-  const produto = document.getElementById("produto");
-  const quantidade = Number(document.getElementById("quantidade").value);
-
-  const precoMilheiro = Number(produto.selectedOptions[0].dataset.preco);
-  const pesoMilheiro = Number(produto.selectedOptions[0].dataset.peso);
-
-  const pesoTotal = quantidade * pesoMilheiro;
-  const valorTotal = quantidade * precoMilheiro;
-
   const resultado = document.getElementById("resultado");
 
-  // Formatação brasileira
-const valorFormatado = valorTotal.toLocaleString("pt-BR", {
-  style: "currency",
-  currency: "BRL"
-});
+  const valorTotal = 1234.56;
+  const pesoTotal = 678;
 
-
-  if (pesoTotal < 500) {
-    resultado.innerHTML = `
-      ❌ Pedido mínimo: <strong>500 kg</strong><br>
-      Peso atual: <strong>${pesoTotal} kg</strong>
-    `;
-    return;
-  }
-
-  const mensagem = encodeURIComponent(
-    `Pedido realizado:
-Produto: ${produto.options[produto.selectedIndex].text}
-Quantidade: ${quantidade} milheiros
-Peso total: ${pesoTotal} kg
-Valor total: ${valorFormatado}`
-  );
+  const valorFormatado = valorTotal.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  });
 
   resultado.innerHTML = `
-    resultado.innerHTML = `
-  <p><strong>Peso total:</strong> ${pesoTotal} kg</p>
-  <p><strong>Valor total:</strong> ${valorFormatado}</p>
-`;
-
-    <a href="https://wa.me/5531990728646?text=${mensagem}" target="_blank">
-      Enviar pedido pelo WhatsApp
-    </a>
+    <p>Peso total: ${pesoTotal} kg</p>
+    <p>Valor total: ${valorFormatado}</p>
   `;
 }
